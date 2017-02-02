62c88b8d2cd3c637b34a752d7a12a8f88ee39f00
Worn Jerabek Wiltse Architects <WJW@Worns-MacBook-Pro.local>
2017-02-02 01:53:12 -0600

    Java Homework

---
+++ js/script.js
@@ -0,0 +1,28 @@
+console.log('window is loaded')
+
+//1. Data Types //
+
+var x = 3.14; //Number //
+
+var y = 'Jeff Sablack'; //String //
+
+var z = ["2", "3", "5", "7", "11"]; //Array //
+
+var myNum = 87; //Number //
+
+var takingWDI = true; //boolean //
+
+//2. Array Manipulation //
+
+var places = ["Where am I going?", "Japan", "Costa Rica", "8"];
+
+console.log('License says I am 22')
+
+var drinkingAge = 22
+
+if (drinkingAge >= 21) {
+	console.log('Down the Hatch')
+}
+else if (drinkingAge < 21) {
+	console.log('Too Pretty to go to Jail')
+}
